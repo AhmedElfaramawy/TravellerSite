06b386ebb05350b1b38e84455a9173d532fa1754
AhmedElfaramawy <89198038+AhmedElfaramawy@users.noreply.github.com>
2025-05-24 19:25:08 +0300

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,9 +1,9 @@
 <?php
 // Try first with the custom port 3307
-$host = "127.0.0.1:3307";
-$user = "root"; // Default username
-$password = ""; // Leave empty if no password is set
-$database = "flight_booking"; // Make sure this matches your imported database name
+$host = "sql310.infinityfree.com";
+$user = "if0_39069649"; // Default username
+$password = "DlJnpN9nSR9h"; // Leave empty if no password is set
+$database = "if0_39069649_flight_booking"; // Make sure this matches your imported database name
 
 // إنشاء الاتصال بقاعدة البيانات
 $conn = new mysqli($host, $user, $password, $database);
@@ -11,7 +11,7 @@ $conn = new mysqli($host, $user, $password, $database);
 // التحقق من نجاح الاتصال
 if ($conn->connect_error) {
     // If connection fails, try with the default port 3306
-    $host = "127.0.0.1:3306";
+    $host = "sql310.infinityfree.com";
     $conn = new mysqli($host, $user, $password, $database);
     if ($conn->connect_error) {
         die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);
